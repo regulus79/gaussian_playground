880bf9a62c9480f769b7333a6a4681c60c7ae6cf
regulus79 <117475203+regulus79@users.noreply.github.com>
2026-04-12 09:09:07 -0400

    L'Hopital's rule for distance=0

--- gaussian_tools.py
+++ gaussian_tools.py
@@ -70,26 +70,50 @@ def gaussianCoulombIntegral(gaussianPos, exponent, derivativeXPower, derivativeY
 
 	# Find the nth derivative with respect to the distance
 	overallDerivative = 0
-	for derivativeOrder in range(totalDerivatives + 1):
-		derivativeExpansionCoeff = coulombDerivativeExpansionCoeff(derivativeOrder, totalDerivatives)
-		oneOverDistancePower = totalDerivatives - derivativeOrder + 1
-		functionDerivative = None
-		if derivativeOrder == 0:
-			functionDerivative = integral1 - integral2
-		else:
-			functionDerivative = hermitePolynomial(derivativeOrder - 1, exponent, distance) * (math.exp(-exponent * distance**2) + math.exp(-exponent * distance**2))
-		#print("derivative order", derivativeOrder, "coeff", derivativeExpansionCoeff, "1/x power", oneOverDistancePower, "derivative", functionDerivative)
-		overallDerivative += derivativeExpansionCoeff * (1/distance) ** oneOverDistancePower * functionDerivative
+	if distance != 0:
+		for derivativeOrder in range(totalDerivatives + 1):
+			derivativeExpansionCoeff = coulombDerivativeExpansionCoeff(derivativeOrder, totalDerivatives)
+			oneOverDistancePower = totalDerivatives - derivativeOrder + 1
+			functionDerivative = None
+			if derivativeOrder == 0:
+				functionDerivative = integral1 - integral2
+			else:
+				functionDerivative = hermitePolynomial(derivativeOrder - 1, exponent, distance) * (math.exp(-exponent * distance**2) + math.exp(-exponent * distance**2))
+			#print("derivative order", derivativeOrder, "coeff", derivativeExpansionCoeff, "1/x power", oneOverDistancePower, "derivative", functionDerivative)
+			overallDerivative += derivativeExpansionCoeff * (1/distance) ** oneOverDistancePower * functionDerivative
+	else:
+		# L'Hopital's rule as distance approaches 0
+		# Multiply all terms by some pwoer of distance tog et it all as a single fraction, (f(x) + xf'(x) + x^2f''(x) ...) / x^n kind of thing
+		# Then we need to take n derivatives of top and bottom to to lhopital's rule
+		# for the bottom, it's just n!
+		# for the top, the only terms which reamin after pluggin in distance=0 are the ones not multiplied by x.
+		# There are n choose i ways to get those kinds of terms for each term, they all turn out to be n'th derivatives of f. And with some i! from the derivaitves of the x on the side.
+		# This is so confusing.
+		# Essentially it comes down to summing up (derivativeExpansionCoeff * (n choose i) * i!) * (n'th derivative of f)
+		nthFunctionDerivative = hermitePolynomial(totalDerivatives, exponent, 0) * (math.exp(-exponent * 0**2) + math.exp(-exponent * 0**2))
+		
+		oneOverDistancePower = totalDerivatives + 1
+
+		numeratorSum = 0
+		for derivativeOrder in range(totalDerivatives + 1):
+			derivativeExpansionCoeff = coulombDerivativeExpansionCoeff(derivativeOrder, totalDerivatives)
+			numeratorSum += derivativeExpansionCoeff * math.comb(totalDerivatives + 1, derivativeOrder) * math.factorial(derivativeOrder) * nthFunctionDerivative
+			#print(derivativeExpansionCoeff, math.comb(totalDerivatives + 1, derivativeOrder), math.factorial(derivativeOrder))
+			
+		lhopital = numeratorSum / math.factorial(oneOverDistancePower)
+		#print(totalDerivatives, oneOverDistancePower, nthFunctionDerivative, numeratorSum, lhopital)
+		overallDerivative = lhopital
 	#print(overallCoeff / distance * (integral1 - integral2))
 	
+	if distance != 0:
+		direction = offset / distance
+		derivativeDistanceWrtX = np.sum(np.array([1,0,0]) * direction)
+		derivativeDistanceWrtY = np.sum(np.array([0,1,0]) * direction)
+		derivativeDistanceWrtZ = np.sum(np.array([0,0,1]) * direction)
+		#print(derivativeDistanceWrtX, derivativeDistanceWrtY, derivativeDistanceWrtZ)
+		#print(overallCoeff * overallDerivative, (derivativeDistanceWrtX**derivativeXPower * derivativeDistanceWrtY**derivativeYPower * derivativeDistanceWrtZ**derivativeZPower))
 
-
-	direction = offset / distance
-	derivativeDistanceWrtX = np.sum(np.array([1,0,0]) * direction)
-	derivativeDistanceWrtY = np.sum(np.array([0,1,0]) * direction)
-	derivativeDistanceWrtZ = np.sum(np.array([0,0,1]) * direction)
-	#print(derivativeDistanceWrtX, derivativeDistanceWrtY, derivativeDistanceWrtZ)
-	#print(overallCoeff * overallDerivative, (derivativeDistanceWrtX**derivativeXPower * derivativeDistanceWrtY**derivativeYPower * derivativeDistanceWrtZ**derivativeZPower))
-
-	return  overallCoeff * overallDerivative * derivativeDistanceWrtX**derivativeXPower * derivativeDistanceWrtY**derivativeYPower * derivativeDistanceWrtZ**derivativeZPower
+		return overallCoeff * overallDerivative * derivativeDistanceWrtX**derivativeXPower * derivativeDistanceWrtY**derivativeYPower * derivativeDistanceWrtZ**derivativeZPower
+	else:
+		return overallCoeff * overallDerivative
 
